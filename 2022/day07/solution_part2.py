44047db2ba85d9d6040dd405227bf478eee2cadc
bakhtaki <hamidreza.bakhtaki@gmail.com>
2022-12-08 11:18:24 +0330

    Day 7 Finished.

---
+++ 2022/day07/solution_part2.py
@@ -0,0 +1,69 @@
+"""Advent of code 2022 day 7 solution."""
+import sys
+from collections import defaultdict
+from functools import lru_cache
+
+
+filename = sys.argv[1]
+with open(filename) as fin:
+    blocks = ("\n" + fin.read().strip()).split("\n$ ")[1:]
+
+path = []
+
+dir_sizes = defaultdict(int)
+children = defaultdict(list)
+
+
+def parse(block):
+    lines = block.split("\n")
+    command = lines[0]
+    outputs = lines[1:]
+
+    parts = command.split(" ")
+    op = parts[0]
+    if op == "cd":
+        if parts[1] == "..":
+            path.pop()
+        else:
+            path.append(parts[1])
+
+        return
+
+    abspath = "/".join(path)
+    assert op == "ls"
+
+    sizes = []
+    for line in outputs:
+        if not line.startswith("dir"):
+            sizes.append(int(line.split(" ")[0]))
+        else:
+            dir_name = line.split(" ")[1]
+            children[abspath].append(f"{abspath}/{dir_name}")
+
+    dir_sizes[abspath] = sum(sizes)
+
+
+for block in blocks:
+    parse(block)
+
+
+# Do DFS
+@lru_cache(None)  # Cache may not be strictly necessary
+def dfs(abspath):
+    size = dir_sizes[abspath]
+    for child in children[abspath]:
+        size += dfs(child)
+    return size
+
+
+# Total size of disk is : 70000K
+unsed = 70000000 - dfs("/")
+required = 30000000 - unsed
+
+answer = 1 << 30
+for abspath in dir_sizes:
+    size = dfs(abspath)
+    if size >= required:
+        answer = min(answer, size)
+
+print(answer)
