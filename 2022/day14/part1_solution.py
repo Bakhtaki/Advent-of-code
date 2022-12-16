12c015677469d03cff72b0f150ce14c8ae0fb0ef
Hamid Reza Bakhtaki <hamidreza.bakhtaki@gmail.com>
2022-12-16 13:51:58 +0330

    Day14 Finished.

---
+++ 2022/day14/part1_solution.py
@@ -0,0 +1,79 @@
+"""Advent of code 2022 day 14 soltion."""
+
+import sys
+
+
+# Simluate the snad fall.
+def simulate_sand():
+    """Simulate the sand fall."""
+    # Globalize the variables.
+    global filled
+
+    x, y = 500, 0
+
+    while y <= max_y:
+        if (x, y + 1) not in filled:
+            y += 1
+            continue
+
+        if (x - 1, y + 1) not in filled:
+            x -= 1
+            y += 1
+            continue
+
+        if (x + 1, y + 1) not in filled:
+            x += 1
+            y += 1
+            continue
+
+        filled.add((x, y))
+        return True
+
+    return False
+
+
+if __name__ == '__main__':
+    input_file = sys.argv[1]
+    with open(input_file, 'r', encoding='utf-8') as file:
+        lines = file.read().strip().splitlines()
+
+    entry_point = (500, 0)
+
+    # Filled Points
+    filled = set()
+
+    # Fill the points from the input_file
+    for line in lines:
+        coordinates = []
+        for coordinate in line.split(' -> '):
+            x, y = map(int, coordinate.split(','))
+            coordinates.append((x, y))
+
+        for i in range(1, len(coordinates)):
+            current_x, current_y = coordinates[i]
+            previous_x, previous_y = coordinates[i - 1]
+
+            if current_y != previous_y:
+                assert current_x == previous_x
+                for y in range(min(current_y, previous_y),
+                               max(current_y, previous_y) + 1):
+                    filled.add((current_x, y))
+            if current_x != previous_x:
+                assert current_y == previous_y
+                for x in range(min(current_x, previous_x),
+                               max(current_x, previous_x) + 1):
+                    filled.add((x, current_y))
+
+    max_y = max([coordinate[1] for coordinate in filled])
+
+    # Part 1.
+    part_1 = 0
+
+    while True:
+        result = simulate_sand()
+
+        if not result:
+            break
+        part_1 += 1
+
+    print(f'Part 1: {part_1}')
