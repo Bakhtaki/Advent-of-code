b09b652742d0bf16573186d31c393f7dc78e6cd9
Hamid Reza Bakhtaki <hamidreza.bakhtaki@gmail.com>
2022-12-21 11:20:19 +0330

    Day16 Finished.

---
+++ 2022/day16/solution.py
@@ -0,0 +1,44 @@
+"""Advent of Code 2022 Day16 Solution."""
+
+import sys
+from functools import cache
+
+
+@cache
+def solve(pos, time, opened, ele_wait=False):
+    if time == 0:
+        if ele_wait:
+            return solve("AA", 26, opened)
+        return 0
+
+    score = max(solve(n, time - 1, opened, ele_wait) for n in maps[pos])
+
+    if flows[pos] > 0 and pos not in opened:
+        new_opened = set(opened)
+        new_opened.add(pos)
+        score = max(
+            score,
+            (time - 1) * flows[pos]
+            + solve(pos, time - 1, frozenset(new_opened), ele_wait),
+        )
+    return score
+
+
+with open(sys.argv[1], "r") as f:
+    lines = f.readlines()
+
+flows = {}
+maps = {}
+
+for line in lines:
+    # Valve RU has flow rate=0; tunnels lead to valves YH, ID
+    toks = line.split(" ")
+    valve = toks[1]
+    flows[valve] = int(toks[4].split("=")[1].strip(";"))
+    maps[valve] = [t.strip(",\n") for t in toks[9:]]
+
+part1 = solve("AA", 30, frozenset())
+print(f"Part 1: {part1}")
+
+part2 = solve("AA", 26, frozenset(), True)
+print(f"Part 2: {part2}")
