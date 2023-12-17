477e532375bd28781187bda48c4c15bdde540e35
bakhtaki <hamidreza.bakhtaki@gmail.com>
2023-12-17 07:51:07 +0330

    Day 10 Part 1

--- 2023/Day10/Solution.py
+++ 2023/Day10/Solution.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 
 
 def get_file_content(file_name):
@@ -17,8 +18,72 @@ def get_file_content(file_name):
     return file_content
 
 
+def get_neighbors(i, j, lines):
+    result = []
+    for di, dj in list(get_direct_neighbors(i, j, lines)):
+        ii, jj = i + di, j + dj
+
+        if not (0 <= ii < len(lines) and 0 <= jj < len(lines[0])):
+            continue
+        result.append((ii, jj))
+    return result
+
+
+def get_direct_neighbors(i, j, lines):
+    result = []
+    if lines[i][j] == 'S':
+        for di, dj in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            ii, jj = i + di, j + dj
+
+            if not (0 <= ii < len(lines) and 0 <= jj < len(lines[0])):
+                continue
+            if (i, j) in list(get_neighbors(ii, jj, lines)):
+                result.append((di, dj))
+        return result
+
+    else:
+        result = {
+            '|': [(1, 0), (-1, 0)],
+            '-': [(0, 1), (0, -1)],
+            'L': [(0, 1), (1, 0)],
+            'J': [(-1, 0), (0, -1)],
+            'F': [(1, 0), (0, 1)],
+            '7': [(1, 0), (0, -1)],
+            '.': []
+        }[lines[i][j]]
+        return result
+
+
 def part1(data):
-    print("Part 1")
+    lines = data.splitlines()
+
+    # Find Start Position
+    si, sj = None, None
+    for i, line in enumerate(lines):
+        if 'S' in line:
+            si, sj = i, line.index('S')
+            break
+
+    # BFS to find shortest path
+    visited = set()
+    destinations = {}
+
+    q = deque([((si, sj), 0), ])
+
+    while len(q) > 0:
+        top, dist = q.popleft()
+        if top in visited:
+            continue
+        visited.add(top)
+        destinations[top] = dist
+
+        for nbr in list(get_neighbors(*top, lines)):
+            if nbr in visited:
+                continue
+            q.append((nbr, dist + 1))
+
+    answer = max(destinations.values())
+    print(f'Part 1: {answer}')
 
 
 def part2(data):
