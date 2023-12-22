e1c14b253edbc6a1218906b37a01573c4e62507f
bakhtaki <hamidreza.bakhtaki@gmail.com>
2023-12-22 11:34:29 +0330

    Day13 Finished.

---
+++ 2023/Day10/Solution_Part2.py
@@ -0,0 +1,99 @@
+with open("./input.txt") as fin:
+    lines = fin.read().strip().split("\n")
+
+n, m = len(lines), len(lines[0])
+
+
+def get_nbrs(i, j):
+    res = []
+    for di, dj in list(get_dnbrs(i, j)):
+        ii, jj = i + di, j + dj
+        if not (0 <= ii < n and 0 <= jj < m):
+            continue
+        res.append((ii, jj))
+    return res
+
+
+dirs = {
+    "|": [(1, 0), (-1, 0)],
+    "-": [(0, 1), (0, -1)],
+    "L": [(-1, 0), (0, 1)],
+    "J": [(-1, 0), (0, -1)],
+    "7": [(1, 0), (0, -1)],
+    "F": [(1, 0), (0, 1)],
+    ".": [],
+}
+
+
+def get_dnbrs(i, j):
+    res = []
+    if lines[i][j] == "S":
+        for di, dj in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            ii, jj = i + di, j + dj
+            didjs = []  # Values of di and dj that link up
+            if not (0 <= ii < n and 0 <= jj < m):
+                continue
+
+            if (i, j) in list(get_nbrs(ii, jj)):
+                didjs.append((i, j))
+                res.append((di, dj))
+
+        # Find which character corresponds to this one
+        # "ds" for "dirs"
+        for char, ds in dirs.items():
+            if sorted(ds) == sorted(didjs):
+                break
+
+        lines[i].replace("S", char)
+
+        return res
+
+    else:
+        return dirs[lines[i][j]]
+
+
+si, sj = None, None
+for i, line in enumerate(lines):
+    if "S" in line:
+        si, sj = i, line.index("S")
+        break
+
+
+# Do a BFS
+visited = set()
+dists = {}
+stack = [(si, sj)]
+while len(stack) > 0:
+    top = stack.pop()
+    if top in visited:
+        continue
+    visited.add(top)
+
+    for nbr in list(get_nbrs(*top)):
+        if nbr in visited:
+            continue
+        stack.append(nbr)
+
+
+# Count the number of "inversions" in a row
+def count_invs(i, j):
+    # Everything up to (but not including) j in line i
+    line = lines[i]
+    count = 0
+    for k in range(j):
+        if not (i, k) in visited:
+            continue
+        count += line[k] in {"J", "L", "|"}
+
+    return count
+
+
+ans = 0
+for i, line in enumerate(lines):
+    for j in range(m):
+        if not (i, j) in visited:
+            invs = count_invs(i, j)
+            if invs % 2 == 1:
+                ans += 1
+
+print(ans)
--- 2023/Day13/Solution.py
+++ 2023/Day13/Solution.py
@@ -1,4 +1,5 @@
 import sys
+from copy import deepcopy
 
 
 def get_file_content(file_name):
@@ -17,12 +18,143 @@ def get_file_content(file_name):
     return file_content
 
 
+def check_pattern(grid):
+    """
+    Checks if the pattern whether reflected Vertically, Horizontally
+
+    Args:
+        grid (list): The grid to check.
+    output:
+        Int
+    """
+    sum = 0
+    n, m = len(grid), len(grid[0])
+
+    # Horizontal
+    horizontal = -1
+    for i in range(n-1):
+        if check_horizontal(grid, i):
+            horizontal = i
+            break
+
+    # Vertical
+    vertical = -1
+    transposed = transpose(grid)
+
+    for i in range(m-1):
+        if check_horizontal(transposed, i):
+            vertical = i
+            break
+
+    assert horizontal == -1 or vertical == -1
+    sum = vertical + 1 + 100 * (horizontal + 1)
+
+    return sum
+
+
+def check_horizontal(grid, i):
+
+    n, m = len(grid), len(grid[0])
+
+    for j in range(m):
+        for k1 in range(n):
+            k2 = i * 2+1 - k1
+            if not (0 <= k2 < n):
+                continue
+            if grid[k1][j] != grid[k2][j]:
+                return False
+    return True
+
+
+def transpose(grid):
+    """
+    Transposes a grid.
+
+    Args:
+        grid (list): The grid to transpose.
+
+    Returns:
+        list: The transposed grid.
+    """
+
+    return list(zip(*grid))
+
+
+def find_reflection(grid, ignore=(-1, -1)):
+    n, m = len(grid), len(grid[0])
+
+    # Horizontal
+    horizontal = -1
+    for i in range(n-1):
+        if i != ignore[0] and check_horizontal(grid, i):
+            horizontal = i
+            break
+
+    # Vertical
+    vertical = -1
+    transposed = transpose(grid)
+    for j in range(m-1):
+        if j != ignore[1] and check_horizontal(transposed, j):
+            vertical = j
+            break
+
+    return (horizontal, vertical)
+
+
+def fix_reflection(grid):
+    """
+    Fixes the reflection of a grid.
+
+    Args:
+        grid (list): The grid to fix.
+
+    Returns:
+        list: The fixed grid.
+    """
+
+    n, m = len(grid), len(grid[0])
+
+    original_reflection = find_reflection(grid)
+    print(original_reflection)
+
+    for i in range(n):
+        for j in range(m):
+            grid_copy = deepcopy(grid)
+            grid_copy[i][j] = '.' if grid_copy[i][j] == '#' else '#'
+
+            new_reflection = find_reflection(grid_copy, original_reflection)
+
+            if new_reflection not in [original_reflection, (-1, -1)]:
+                if new_reflection[0] != -1:
+                    answer = (new_reflection[0] + 1) * 100
+                else:
+                    assert new_reflection[1] != -1
+                    answer = (new_reflection[1]) + 1
+
+                return answer
+
+
 def part1(data):
-    print("Part 1")
+    pattern = [line.split() for line in data.split('\n\n')]
+    answer = 0
+
+    for grid in pattern:
+        result = check_pattern(grid)
+        answer += result
+
+    print(f'Answer: {answer}')
 
 
 def part2(data):
-    return data
+    pattern = [[line.split() for line in grid.split('\n')]
+               for grid in data.split('\n\n')]
+    answer = 0
+
+    for grid in pattern:
+        result = fix_reflection(grid)
+        answer += result
+
+    print(f'Answer: {answer}')
 
 
 if __name__ == '__main__':
